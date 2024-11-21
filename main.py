75dee7165b7926cd868c14eccf606ac9d6b44eb2
AbdouMurad <abdoumurad1522@gmail.com>
2024-11-21 12:45:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,3 @@ if __name__ == "__main__":
     #uncomment the following line when you want to use it but keep it off on default because it charges per usage
     #print(TranscribeSpeech('Recorded.wav'))
 
-    #TEST
