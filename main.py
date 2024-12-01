14498ad01e1098827cc6491cc4370ee7c1735abd
Betts6430 <avery.bettesworth643@gmail.com>
2024-12-01 14:25:44 -0700

    idk

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from ProcessRequest import TranscribeSpeech, InterpertTextObject, initializeTree
 from Audio import Record,Speak
+from Memory import log_data
 
 
 def main():
+    first_text = True
+
     Speak("Hello my name is Nova, how can I help you today?")
     TextProcessor = InterpertTextObject()
     while True:      
@@ -12,8 +15,16 @@ def main():
         TextProcessor = InterpertTextObject()
 
         UserText = TranscribeSpeech("Recorded.wav")
+        SystemText = "Hello"  # Sample text for now
+
+        # Logs user and system inputs into memory_log.txt
+        log_data("User", UserText, first_text)
+        log_data("System", SystemText, first_text)
+
         FunctionRequest = TextProcessor.interpertText(UserText,TextProcessor.system_message_text_to_funcitons,TextProcessor.prompt_text_to_functions)
         TextProcessor.splitFunctions(FunctionRequest)
+
+        first_text = False
 if __name__ == "__main__":
     main()                            
                     
